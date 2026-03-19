7582845c4a9d999b6c98995421270e442d94ad2e
TechPanelGM <giomook2003@gmail.com>
2026-03-19 12:03:26 +0100

    ceres: Configure dynamic partitions

--- device.mk
+++ device.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
