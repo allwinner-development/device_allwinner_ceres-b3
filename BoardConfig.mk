02f52b856689c91878f336ec0041f64b18540c62
TechPanelGM <giomook2003@gmail.com>
2026-03-20 11:45:30 +0100

    ceres: Enable support for EXT4 & F2FS formatted userdata

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,8 @@ TARGET_BOARD_PLATFORM := ceres
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.sun50iw10p1
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := false
